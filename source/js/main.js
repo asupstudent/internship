d51b6f2658064859732a43a24c4554573c46f952
asupstudent <asupstudent@yandex.ru>
2024-01-11 22:39:17 +0300

    Размечает слайдер и оживляет его

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,17 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+import Swiper from 'swiper';
+import {Pagination} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const slider = document.querySelector('[data-swiper="slider"]');
+const sliderPagination = document.querySelector('[data-swiper-pagination="slider"]');
+
+// eslint-disable-next-line no-unused-vars
+const swiper = new Swiper(slider, {
+  modules: [Pagination],
+  pagination: {
+    el: sliderPagination,
+    clickable: true,
+  },
+  loop: true,
+});
