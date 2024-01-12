d7ca09f28b24929081e07f30d1f784890bd19c94
asupstudent <asupstudent@yandex.ru>
2024-01-12 15:24:07 +0300

    Удаляет ios-checker и ios-vh-fix

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,6 @@
-import {iosVhFix} from './utils/ios-vh-fix';
 import {initSliderHero} from './modules/sliders/init-slider-hero';
 
 window.addEventListener('DOMContentLoaded', () => {
-  iosVhFix();
   initSliderHero();
 
   window.addEventListener('load', () => {
