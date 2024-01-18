107b5c44d8cf3bfd1b0a721ded6b28aa7caded24
asupstudent <asupstudent@yandex.ru>
2024-01-18 08:49:55 +0300

    Подключает валидацию формы, кастомный селект, чекбокс, инпут

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,7 @@
 import './utils/scroll-lock';
 import './utils/focus-lock';
+import {CustomSelect} from './vendor/custom-select/custom-select';
+import {Form} from './vendor/form-validate/form';
 import {initBurgerMenu} from './modules/init-burger-menu';
 import {initAccordions} from './modules/init-accordions';
 import {initModals} from './modules/init-modals';
@@ -12,5 +14,12 @@ window.addEventListener('DOMContentLoaded', () => {
     initBurgerMenu();
     initAccordions();
     initModals();
+
+    const select = new CustomSelect();
+    select.init();
+
+    const form = new Form();
+    window.form = form;
+    form.init();
   });
 });
