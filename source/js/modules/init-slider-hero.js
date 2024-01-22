c82e190f99a3aecc7aea84e4b007286f2d31edd0
asupstudent <asupstudent@yandex.ru>
2024-01-22 12:35:29 +0300

    Добавляет пагинацию на каждом слайде

--- source/js/modules/init-slider-hero.js
+++ source/js/modules/init-slider-hero.js
@@ -1,27 +1,38 @@
-import Swiper from 'swiper/bundle';
+import Swiper from 'swiper';
+import { Autoplay, Pagination } from 'swiper/modules';
 
 const heroSlider = document.querySelector('[data-swiper="hero"]');
-const heroSliderPagination = document.querySelector('[data-swiper="hero-pagination"]');
 
-const setSliderHero = () => new Swiper(heroSlider, {
-  pagination: {
-    el: heroSliderPagination,
-    clickable: true,
-    type: 'bullets',
-    bulletElement: 'div',
-    bulletClass: 'hero__pagination-bullet',
-    bulletActiveClass: 'hero__pagination-bullet--active',
-  },
-  // autoplay: {
-  //   delay: 3000,
-  // },
-  loop: true,
-  breakpoints: {
-    1440: {
-      allowTouchMove: false,
+const setSliderHero = () => {
+  const slider = new Swiper(heroSlider, {
+    observer: true,
+    modules: [Autoplay, Pagination],
+    pagination: {
+      el: '.swiper-slide-active .hero__pagination',
+      clickable: true,
+      type: 'bullets',
+      bulletElement: 'div',
+      bulletClass: 'hero__pagination-bullet',
+      bulletActiveClass: 'hero__pagination-bullet--active',
     },
-  },
-});
+    autoplay: {
+      delay: 3000,
+    },
+    loop: true,
+    breakpoints: {
+      1440: {
+        allowTouchMove: false,
+      },
+    },
+    on: {
+      slideChangeTransitionStart: function () {
+        slider.pagination.init();
+        slider.pagination.render();
+        slider.pagination.update();
+      }
+    }
+  });
+};
 
 const initSliderHero = () => {
   if (document.body.contains(heroSlider)) {
