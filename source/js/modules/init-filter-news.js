7b8b1fb8739c22d16f410543257ca633ecc51fc9
asupstudent <asupstudent@yandex.ru>
2024-02-05 13:09:04 +0300

    Настраивает фильтр осталось его к swiper-у прикрутить

--- source/js/modules/init-filter-news.js
+++ source/js/modules/init-filter-news.js
@@ -1,10 +1,20 @@
 const isActive = 'is-active';
 const buttonContainer = document.querySelector('[data-news="button-container"]');
-const newsBtns = buttonContainer.querySelectorAll('[data-name="news-button"]');
+const newsButtons = buttonContainer.querySelectorAll('[data-name="news-button"]');
+const newsSlides = document.querySelectorAll('[data-news="slide"]');
 
 const onNewsButtonClick = (activeButton) => {
-  newsBtns.forEach((button) => button.classList.remove(isActive));
+  newsButtons.forEach((button) => button.classList.remove(isActive));
   activeButton.classList.add(isActive);
+};
+
+const updateNewsSlider = (filter) => {
+  newsSlides.forEach((slide) => {
+    const slideFilter = slide.dataset.filter;
+    const isShowSlide = filter === 'all' || slideFilter === filter;
+    // eslint-disable-next-line no-console
+    console.log(isShowSlide);
+  });
 };
 
 const onButtonContainerClick = (evt) => {
@@ -14,10 +24,12 @@ const onButtonContainerClick = (evt) => {
       onNewsButtonClick(currentButton);
     }
   }
+  const filter = currentButton.dataset.filter || 'all';
+  updateNewsSlider(filter);
 };
 
 export const initNewsFilter = () => {
-  if (buttonContainer && newsBtns) {
+  if (buttonContainer && newsButtons) {
     buttonContainer.addEventListener('click', onButtonContainerClick);
   }
 };
